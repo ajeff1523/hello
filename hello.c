5c527cac71894b68d7075c26c8650a7f2806a2cc
Austin Jeffrey <ajeffrey1523@gmail.com>
2019-01-17 12:26:29 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Austin Jeffrey, Bethany Krull
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Finance\n");
+
+  return 0;
+}
